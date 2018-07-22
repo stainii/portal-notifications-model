a80fa98297e1c8f49147598706fe007677347bd7
stijnhooft@hotmail.com <stijnhoof@hotmail.com>
2018-07-22 17:17:37 +0200

    Make the difference between the internal and external url of an action

--- src/main/java/be/stijnhooft/portal/notifications/model/NotificationAction.java
+++ src/main/java/be/stijnhooft/portal/notifications/model/NotificationAction.java
@@ -2,15 +2,30 @@ package be.stijnhooft.portal.notifications.model;
 
 import lombok.*;
 
-@ToString @EqualsAndHashCode
+@ToString
+@EqualsAndHashCode
 @Getter
-@AllArgsConstructor @NoArgsConstructor
+@NoArgsConstructor @AllArgsConstructor
 public class NotificationAction {
 
-  /** url that needs to be opened when the user taps the notification **/
-  private String url;
 
-  /** the text that should appear on the action button of the notification **/
-  private String text;
+    /**
+     * Url that needs to be opened when the user taps the notification.
+     * This is a url that point to the notification app. The app will mark the notification as read,
+     * and redirect the user to the actual internal url.
+     **/
+    @NonNull
+    private String url;
+
+    /**
+     * the text that should appear on the action button of the notification
+     **/
+    @NonNull
+    private String text;
+
+    /**
+     * url where the user actually needs to be forwarded to, when he/she taps on the notification.
+     **/
+    private String internalUrl;
 
 }
